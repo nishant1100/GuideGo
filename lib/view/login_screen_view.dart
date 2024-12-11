342057d1a19f2cc5b4c137f6ae73af4c427657a5
nishant1100 <nishantstha1010@gmail.com>
2024-12-11 18:54:45 +0545

    login pagea added

---
+++ lib/view/login_screen_view.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+
+
+class LoginScreenView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      body: Container(
+        width: screenWidth,
+        height: screenHeight,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [const Color(0xFF9C27B0), const Color.fromRGBO(33, 150, 243, 1)],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: OrientationBuilder(
+          builder: (context, orientation) {
+            return SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    SizedBox(height: screenHeight * 0.05),
+                    Image.asset(
+                      'assets/logo/guide_go.png', // Replace with your logo asset path
+                      height: orientation == Orientation.portrait
+                          ? screenHeight * 0.3
+                          : screenHeight * 0.5, // Adjust logo size
+                    ),
+                    SizedBox(height: 0),
+                    Text(
+                      'Happy to see you! Please enter your email and password to login to your account.',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: screenWidth * 0.04, // Responsive font size
+                      ),
+                    ),
+                    SizedBox(height: 30),
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: 'Email Address',
+                        labelStyle: TextStyle(color: Colors.white),
+                        filled: true,
+                        fillColor: Colors.white.withOpacity(0.1),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide.none,
+                        ),
+                      ),
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    SizedBox(height: 20),
+                    TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: 'Your Password',
+                        labelStyle: TextStyle(color: Colors.white),
+                        filled: true,
+                        fillColor: Colors.white.withOpacity(0.1),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: BorderSide.none,
+                        ),
+                      ),
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      'Forgot Password?',
+                      textAlign: TextAlign.right,
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    SizedBox(height: 100),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFF9C27B0),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        minimumSize: Size(
+                          screenWidth * 0.6, // 60% of screen width
+                          50, // Fixed height
+                        ),
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 20,
+                          vertical: orientation == Orientation.portrait ? 15 : 10,
+                        ),
+                      ),
+                      child: Text(
+                        'Sign In',
+                        style: TextStyle(
+                          fontSize: screenWidth * 0.045, // Dynamic font size
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 0),
+                    TextButton(
+                      onPressed: () {
+                      //  
+                      },
+                      child: Text(
+                        "Don't have an account? Create account",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    SizedBox(height: screenHeight * 0.05),
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
