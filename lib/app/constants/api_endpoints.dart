5b84e671e3c8ecd6a0de03a2ad61cda88d673c49
nishant1100 <nishantstha1010@gmail.com>
2025-03-04 23:07:27 +0545

    api endpoints updated and minor chnages on terma and conditon

--- lib/app/constants/api_endpoints.dart
+++ lib/app/constants/api_endpoints.dart
@@ -3,8 +3,8 @@ class ApiEndpoints {
 
   static const Duration connectionTimeout = Duration(seconds: 1000);
   static const Duration receiveTimeout = Duration(seconds: 1000);
-  // static const String baseUrl = "http://10.0.2.2:3000/api";
-    static const String baseUrl = "http://192.168.1.64:3000/api";
+  static const String baseUrl = "http://10.0.2.2:3000/api";
+    //static const String baseUrl = "http://192.168.1.64:3000/api";
 
   //=====================Auth Routes===============================
   static const String registerUser="/auth/register";
